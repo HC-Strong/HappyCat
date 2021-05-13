902d0286b4772d6441e888b093f6c9bca4b9733c
HC-Strong <stronghannahc@gmail.com>
2021-05-12 22:36:27 -0700

    Basic Setup
    Basic HTML layout with styled canvas, Javascript file with names of first functions to work on and required global variables added

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+// Global constants and variables
+let purrCounter = 0;
+const winDuration = 10;
+
+// catStatuses don't autocomplete in Atom editor
+const catStatus = {
+  good:"GOOD",
+  bad:"BAD",
+  meh:"NEUTRAL"
+};
+
+// TO DO: Event Listener for mouse movement also needs to be created
+
+
+// Ryan - trace user's pet path
+function startPet(e) {
+
+}
+
+function petOn(e) {
+
+  checkRegion(coordinates); // Let Hannah know how they're formatted
+}
+
+function endPet(e) {
+
+}
+console.log(catStatus.bad)
+function test() {
+
+}
+
+// Hannah - takes coordinates from petOn(), sends catStatus to purr() based on what region the coordinates are in
+function checkRegion(coordinates) {
+
+}
+
+
+// Lizzy - if status is good, purr and increment purrCounter and PURR audio
+// Takes a catStatus as an input
+function purr(status) {
+
+}
+
+// Lizzy - if purr counter = winDuration, purr image (or standin) appears
+function win() {
+
+}
